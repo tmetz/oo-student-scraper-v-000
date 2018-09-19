772c42a4875faf52fc63007504904635d609b628
Tammy Metz <tammy@tammymetz.com>
2018-09-19 23:01:11 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -15,8 +15,8 @@ class Scraper
       # student-card
       #binding.pry
       person_hash = {
-        :location => person.css("div.student-card div.card-text-container.p").text,
-        :name => person.css("div.student-card div card-text-container.h4").text,
+        :location => person.css("div.student-card div.card-text-container p").text,
+        :name => person.css("div.student-card div card-text-container h4").text,
         :profile_url => person.css("div.student-card a").attribute("href")
       }
       people << person_hash
